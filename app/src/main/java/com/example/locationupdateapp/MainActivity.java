9dd16f88a37937d9ea8c946f3f38ff61a443967e
Jack <jhal0010@student.monash.edu.au>
2022-03-27 10:37:06 +1100

    UI layout created. Variables declared in MainActivity. String resources created.

--- app/src/main/java/com/example/locationupdateapp/MainActivity.java
+++ app/src/main/java/com/example/locationupdateapp/MainActivity.java
@@ -3,8 +3,25 @@ package com.example.locationupdateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //The distance from previous position required for location update to occur
+    public static final double UPDATE_DISTANCE = 10;
+
+    //The current distance from previous position as a multiple of UPDATE_DISTANCE
+    private int notifyThreshold;
+
+    //The user's initial position
+    private String previousPosition;
+
+    //Reference to the text display
+    private TextView textDisplay;
+
+    //Reference to the button
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
